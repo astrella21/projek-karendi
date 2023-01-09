d8b8309becbd666a6604c0df9820f7e50fd973ac
astrella21 <anantaastrella1@gmail.com>
2023-01-09 21:07:56 +0700

    projek

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="assets/logo.jpg">
+</head>
+<body>
+    <header id="header">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+            <a class="navbar-brand" href="#">Astrella Elga Ananta</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto me-1 mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link mx-2 active" aria-current="page" href="#header">Project</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="#client">Journey</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="#Project">Service</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="#contact">Contact</a>
+                </li>
+                </ul>
+    </header>
+                <section class="main">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6 col-6 mt-5">
+                                <h1>I'm Frontend Website Developers</h1>
+                                <p class="text">Freelance Frontend Website Developers and Product Designers based on Jakarta, Indonesia</p>
+                                <button type="submit" name="submit" class="kirim">Hire Me</button>
+                            </div>
+                            <div class="col-lg-6 col-6 col-md-12 mt-5">
+                                <img src="foto/img-hero.png" class="w-100">
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="client" data-aos="fade-up" data-aos-offset="300">
+                    <div class="container">
+                        <div class="row mt-5">
+                            <div class="col-12 col-lg-4 my-lg-5">
+                                <div class="my-5">
+                                <h3 class="ms-5">My Recent Client</h3><br>
+                                <p class="text">Perusahaan - perusahaan besar yang sudah bekerja sama dengan saya</p>
+                            </div>
+                                </div>
+                            <div class="col-lg-2 col-md-6 my-lg-5 col-6">
+                                <div class="my-lg-5">
+                                    <img src="foto/logo-tokopedia.png" class="mb-5">
+                                    <img src="foto/logo-google.png"  class="mt-3 mb-5">
+                                </div>
+                            </div>
+                            <div class="col-lg-2 col-md-6 my-lg-5 col-6">
+                                <div class="my-lg-5">
+                                    <img src="foto/logo-gojek.png" class="mb-5">
+                                    <img src="foto/Instagram Logo.png" class="mt-3 mb-5 px-3 ">
+                                </div>
+                            </div>
+                            <div class="col-lg-2 col-md-6 my-lg-5 col-6">
+                                <div class="my-lg-5">
+                                    <img src="foto/logo-spotify.png" class="mb-5">
+                                    <img src="foto/logo-traveloka.png" class="mt-3 mb-5 px-3">
+                                </div>
+                            </div>
+                            <div class="col-lg-2 col-md-6 my-lg-5 col-6">
+                                <div class="my-lg-5">
+                                    <img src="foto/logo-grammarly.png" class="mb-5">
+                                    <img src="foto/logo-facebook.png" class="mt-3 mb-5 px-3">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="Specialities" data-aos="fade-up" data-aos-offset="300">
+                     <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-4 ms-3 my-5">
+                                    <h1 class="ms-5">Specialities</h1><br>
+                                    <p class="text">Beberapa keahlian saya dalam menangani product digital</p>
+                                </div>
+                            </div>
+                            <div class="row d-flex justify-content-between">
+                                <div class="special-box box mt-5 mx-2 pb-4 mb-4 col-lg-3 col-xl-3 col-md-12 col-12 text-primary border-top border-primary border-3 py-4 px-4">
+                                    <img src="foto/ic_point.jpeg" class="fto" alt="">
+                                    <h5 class="pt-4 text-dark">Product Thinking</h5>
+                                    <p class="label">product thinking is a method of analyzing a product using a design thinking framework</p>
+                                </div>
+                                <div class="special-box box mt-5 mx-2 pb-4 mb-4 col-lg-3 col-xl-3 col-md-12 col-12 text-primary border-top border-primary border-3 py-4 px-4">
+                                    <img src="foto/ic_uiux.png" class="fto" alt="">
+                                    <h5 class="pt-4 text-dark">UI/UX Designers</h5>
+                                    <p class="label">Design all screen from client brief and sometimes build the company design system</p>
+                                </div>
+                                <div class="special-box box mt-5 mx-2 pb-4 mb-4 col-lg-3 col-xl-3 col-md-12 col-12 text-primary border-top border-primary border-3 py-4 px-4">
+                                    <img src="foto/ic_frontend.png" class="fto" alt="">
+                                    <h5 class="pt-4 text-dark">Frontend Developer</h5>
+                                    <p class="label">The skill is a slicing all component from design to code and make the  website responsive</p>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                </section>
+                <section id="Project" data-aos="fade-up">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5 col-12 mt-5">
+                                <div class="my-5">
+                                <h1 class="ms-5">My Portfolio Project</h1><br>
+                                <p class="text">Beberapa portfolio yang saya kerjakan untuk client client saya</p>
+                            </div>
+                                </div>
+                            <div class="row justify-content-between mb-md-5 mb-3">
+                                <div class="project col-12 col-lg-6 col-md-6 col-3 mt-5">
+                                    <img src="foto/salinan img-medium1.png" class="w-100">
+                                    <h1>Gaming Store Landing Page</h1>
+                                    <p class="labels pt-3 py-5">Web Designers</p>
+                                </div>
+                                <div class="project col-12 col-lg-6 col-md-6 col-3 mt-5">
+                                    <img src="foto/Salinan img-medium2.png" class="w-100">
+                                    <h1>UI Design Digital Agency</h1>
+                                    <p class="text">UI/UX Design</p>
+                                </div>
+                                </div>
+                                <div class="project col-12 col-lg-4 col-md-4 col-3 pt-3">
+                                    <img src="foto/img-mini1.png" class="w-100">
+                                    <h1>E - Learning Landing Page</h1>
+                                    <p class="text">Web Designers</p>
+                                </div>
+                                <div class="project col-12 col-lg-4 col-md-4 col-3 pt-3">
+                                    <img src="foto/img-mini2.png" class="w-100">
+                                    <h1>Travel App Mobile</h1>
+                                    <p class="text">UI/UX Design</p>
+                                </div>
+                                <div class="project col-12 col-lg-4 col-md-4 col-3 pt-3">
+                                    <img src="foto/img-mini3.png" class="w-100">
+                                    <h1>Booking Villa's App</h1>
+                                    <p class="text">Web Designers</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="contact" data-aos="fade-up">
+                <footer class="menu-nav pt-5">
+                    <div class="pt-5">
+                    <div class="container d-flex">
+                        <div  class="col-8 col-md-5 col-lg-5 pt-5 py-5" href="">
+                            <h3>Astrella Elga Ananta</h3>
+                        </div>
+                        <div class="col-8 pt-5 col-md-5 col-lg-5 ms-5">
+                            <ul class="navbar ms-5">
+                                <a class="nav-link me-3" href="#">Github</a>
+                                <a class="nav-link me-3" href="#">instagram</a>
+                                <a class="nav-link me-3" href="#">Dribble</a>
+                                <a class="nav-link me-3" href="#">Behance</a>
+                                <button type="submit" name="submit" class="kirim">Hire Me</button>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+                </footer>
+                </section>
+</body>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+</html>
